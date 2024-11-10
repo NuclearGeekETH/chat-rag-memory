e8839ca0185e7ce81a288fbd45379974e92d6a31
NuclearGeek <spickett84@yahoo.com>
2024-11-10 13:52:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with gr.Blocks(theme=gr.themes.Soft(), title="LLM with Memory Demo") as demo:
     bot = gr.Chatbot(render=False)
 
     dropdown = gr.Dropdown(
-        ["gpt-4o-2024-08-06", "gpt-4o", "gpt-4o-mini", "chatgpt-4o-latest", "gpt-4-0125-preview", "gpt-4-turbo", "gpt-4-1106-preview", "gpt-4"],
+        ["gpt-4o", "gpt-4o-mini"],
         label = "Model",
         value = "gpt-4o-mini",
         render = False
@@ -16,12 +16,12 @@ with gr.Blocks(theme=gr.themes.Soft(), title="LLM with Memory Demo") as demo:
     system = gr.Textbox(
         lines = 2,
         label = "System Message",
-    value="You are ChatGPT, a large language model trained by OpenAI based on the GPT-4 architecture. " +
-          "You have the ability to store, delete and retrieve memories. " +
-          "Save anything of value and attempt to retrieve a memory when needed or asked. " +
-          "Delete memories on request using the memory_id. " +
-          "Always use memory_id when referencing or managing memories. " +
-          "Make it clear when a memory is being stored, retrieved, or deleted and use the associated memory ID.",
+        value="You are ChatGPT, a large language model trained by OpenAI based on the GPT-4 architecture. " +
+            "You have the ability to store, delete and retrieve memories. " +
+            "Save anything of value and attempt to retrieve a memory when needed or asked. " +
+            "Delete memories on request using the memory_id. " +
+            "Always use memory_id when referencing or managing memories. " +
+            "Make it clear when a memory is being stored, retrieved, or deleted and use the associated memory ID.",
         render = False
         )
 
